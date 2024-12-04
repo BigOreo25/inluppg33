e76d4ab45799734ea62b6962fb5b102d7bc69c5a
BigOreo25 <alts07001@utb.vaxjo.se>
2024-12-04 10:25:37 +0100

    Add project files.

---
+++ inluppg33/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace inluppg33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("tack för att du använder Kreb Bilar. hur många timmar skulle du vilja ha Krebbomobilen?");
+            int timmar = int.Parse(Console.ReadLine());
+            Console.WriteLine("mhm nice nice");
+            int briz = timmar * 80;
+            if (briz <950)
+            {
+                Console.WriteLine("ok det kommer kosta " + briz + "kr. betala helst inom 3.14 sekunder");
+            }
+            else
+            {
+                Console.WriteLine("din krebbomobil kommer kosta 950kr ok hejdå");
+            }
+        }
+    
+    }
+
+}
